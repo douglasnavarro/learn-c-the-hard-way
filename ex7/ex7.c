a6e9ec094f62a977533b9abfbbccfdbea848ff44
Douglas <douglasnavarro94@gmail.com>
2018-01-07 22:19:47 +0000

    break ex7

--- ex7/ex7.c
+++ ex7/ex7.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     printf("You have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
 
-    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+    unsigned long universe_of_defects = 1L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L;
     printf("The entire universe has %ld bugs.\n", universe_of_defects);
 
     double expected_bugs = bugs * bug_rate;
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
     char nul_byte = '\0';
     int care_percentage = bugs * nul_byte;
-    printf("Which means you should care %d%%.\n", care_percentage);
+    printf("Which means you should care %c%%.\n", care_percentage);
 
     return 0;
 }
